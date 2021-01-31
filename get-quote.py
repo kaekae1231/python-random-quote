2e7b4ca4c2907c25dd79181546d47b26a36693d0
Kae-PC <kaejae123@gmail.com>
2021-01-31 23:12:48 +1100

    .

--- get-quote.py
+++ get-quote.py
@@ -6,11 +6,11 @@ def primary():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-
   last = len(quotes) - 1
   rnd = random.randint(0, last)
-
-  print(quotes[rnd])
+  print(len(quotes), quotes)
+  print(quotes[rnd].replace("\n",""))
+  print(quotes[len(quotes)-1].replace("\n",""))
 
 if __name__== "__main__":
   primary()
